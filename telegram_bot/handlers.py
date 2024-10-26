5c78a2b2122c6ea118ea25fc2f0f789b9e2d420f
Ramzil <yazik_2018@mail.ru>
2024-10-26 18:56:27 +0300

    add buttons

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -16,3 +16,23 @@ async def start_handler(msg: Message):
 @router.message(F.text == "Выйти в меню")
 async def message_handler(msg: Message):
     await msg.answer(f"Вы вышли в меню!")
+
+@router.callback_query(F.data == "get_themes")
+async def get_themes(msg: Message):
+    await msg.answer(f"Здесь будут выводиться все темы и еще кое-что!")
+
+@router.callback_query(F.data == "teacher_communication")
+async def get_themes(msg: Message):
+    await msg.answer(f"Здесь можно будет общаться с преподом!")
+
+@router.callback_query(F.data == "get_newsletter")
+async def get_themes(msg: Message):
+    await msg.answer(f"Здесь можно будет отправлять рассылки и еще кое-что!")
+
+@router.callback_query(F.data == "get_vacancies_and_internships")
+async def get_themes(msg: Message):
+    await msg.answer(f"Здесь будут выводиться все стажи и вакансии")
+
+
+
+
